9c6255167354ff5b7380586b0dd49c7aa90139ce
pqx <l.r.slegt@gmail.com>
2022-06-16 03:45:04 +0200

    add JSON webtoken

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
-import jwt from "jsonwebtoken";
+import { useState } from 'react';
+import jwt from 'jsonwebtoken';
 
 export default function Home(req, res) {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-  const [message, setMessage] = useState<string>("You are not logged in");
+  const [message, setMessage] = useState<string>('You are not logged in');
+  const [secret, setSecret] = useState<string>('');
 
   async function submitForm() {
-    const res = await fetch("/api/login", {
-      method: "POST",
+    const res = await fetch('/api/login', {
+      method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ username, password }),
     }).then((t) => t.json());
@@ -21,27 +22,33 @@ export default function Home(req, res) {
     if (token) {
       const json = jwt.decode(token) as { [key: string]: string };
 
-      console.log(json)
-      setMessage(
-        `Welcome ${json.username}, you are ${
-          json.admin ? "an admin" : "not an admin"
-        }`
-      );
+      console.log(json);
+      setMessage(`Welcome ${json.username}, you are ${json.admin ? 'an admin' : 'not an admin'}`);
+
+      const res = await fetch('/api/secret', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token }),
+      }).then((t) => t.json());
+
+      if (res.secretAdminCode) {
+        setSecret(res.secretAdminCode);
+      } else {
+        setSecret('Nothing available');
+      }
     } else {
-        setMessage('Something went wrong')
+      setMessage('Something went wrong');
     }
   }
 
   return (
     <div>
       <h1>{message}</h1>
+      <h1>Secret: {secret}</h1>
       <form>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        <input type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
         <br />
         <input
           type="password"
